458a5e36a3da8e466b2eeb326c00d481b99ced5a
Caio Nunes <caionunesdasilva1@gmail.com>
2021-04-07 15:32:13 -0300

    alteracoes no modulo auxiliar.py, criacao das classes Entidade, Pedido, PedidoCadastro, PedidoLogin, Mensagem, MensagemPrivado, MensagemGrupo

--- auxiliar.py
+++ auxiliar.py
@@ -1,10 +1,7 @@
-
-class Usuario:
-    def __init__(self, codigo: int, nome: str, conexao, endereco):
+class Entidade:
+    def __init__(self, codigo: int, nome: str):
         self._codigo = codigo
         self._nome = nome
-        self._conexao = conexao
-        self._endereco = endereco
 
     @property
     def codigo(self):
@@ -21,6 +18,13 @@ class Usuario:
         # TODO too broad exception clause
         except Exception:
             raise
+
+
+class Usuario(Entidade):
+    def __init__(self, codigo: int, nome: str, conexao, endereco):
+        super().__init__(codigo, nome)
+        self._conexao = conexao
+        self._endereco = endereco
 
     @property
     def conexao(self):
@@ -39,28 +43,11 @@ class Usuario:
             raise
 
 
-class Grupo:
+class Grupo(Entidade):
     def __init__(self, codigo: int, nome: str, dono: Usuario, membros: list):
-        self._codigo = codigo
-        self._nome = nome
+        super().__init__(codigo, nome)
         self._dono = dono
         self._membros = membros
-
-    @property
-    def codigo(self):
-        try:
-            return self._codigo
-        # TODO too broad exception clause
-        except Exception:
-            raise
-
-    @property
-    def nome(self):
-        try:
-            return self._nome
-        # TODO too broad exception clause
-        except Exception:
-            raise
 
     @property
     def dono(self):
@@ -74,6 +61,86 @@ class Grupo:
     def membros(self):
         try:
             return self._membros
+        # TODO too broad exception clause
+        except Exception:
+            raise
+
+
+class Pedido:
+    def __init__(self, nome: str, senha: str):
+        self._nome = nome
+        self._senha = senha
+
+    @property
+    def nome(self):
+        try:
+            return self._nome
+        # TODO too broad exception clause
+        except Exception:
+            raise
+
+    @property
+    def senha(self):
+        try:
+            return self._senha
+        # TODO too broad exception clause
+        except Exception:
+            raise
+
+
+class PedidoCadastro(Pedido):
+    pass
+
+
+class PedidoLogin(Pedido):
+    pass
+
+
+class Mensagem:
+    def __init__(self, mensagem: str, remetente: Usuario):
+        self._mensagem = mensagem
+        self._remetente = remetente
+
+    @property
+    def mensagem(self):
+        try:
+            return self._mensagem
+        # TODO too broad exception clause
+        except Exception:
+            raise
+
+    @property
+    def remetente(self):
+        try:
+            return self._remetente
+        # TODO too broad exception clause
+        except Exception:
+            raise
+
+
+class MensagemPrivado(Mensagem):
+    def __init__(self, mensagem: str, remetente: Usuario, destinatario: Usuario):
+        super().__init__(mensagem, remetente)
+        self._destinatario = destinatario
+
+    @property
+    def destinatario(self):
+        try:
+            return self._destinatario
+        # TODO too broad exception clause
+        except Exception:
+            raise
+
+
+class MensagemGrupo(Mensagem):
+    def __init__(self, mensagem: str, remetente: Usuario, grupo: Grupo):
+        super().__init__(mensagem, remetente)
+        self._grupo = grupo
+
+    @property
+    def grupo(self):
+        try:
+            return self._grupo
         # TODO too broad exception clause
         except Exception:
             raise
